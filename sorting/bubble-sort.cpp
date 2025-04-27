c598dd9401c11d014f14ee569432244c4e906011
Manav Malhotra <roughsheet2002@gmail.com>
2025-04-27 16:46:02 +0530

    added optimized bubble sort

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -4,12 +4,21 @@ using namespace std;
 // Approach 
 // Compare & Swap adjacent elements till the largest one comes at the end of the array
 
+// time complexity sum of naturatl no.s  n + n-1 + n-2 ...
+// so (n^2) which is worst case
+// but it can be optimized o(n) if the array is already in sorted position (imp can be asked)
+
 void bubbleSort(int arr[] , int size) {
     for (int i = 0; i<size-1 ; i++) {
+       bool swapped = false;   // for optimization
        for(int j = 0; j < size - i- 1;  j++) {
         if(arr[j] > arr[j+1]) {
             swap(arr[j] , arr[j+1]);
+            swapped = true;
         }
+       }
+       if(!swapped){      // for optimization
+        break ;
        }
     }
 }
