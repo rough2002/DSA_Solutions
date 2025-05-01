ea8f6caad16a1759ca7c830dc1ee21eb3cb8e08f
Manav Malhotra <roughsheet2002@gmail.com>
2025-05-02 01:10:09 +0530

    union of 2 arrays

---
+++ arrays/union-of-2-array.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> unionOptimal(vector<int> a  , vector<int> b){
+    int n1 = a.size();
+    int n2 = b.size();
+    int i =0;
+    int j =0;
+    vector<int> unionArr;
+
+    while( i < n1 && j < n2) {
+       if(a[i] < b[j]){
+         if(unionArr.size() == 0 || unionArr.back() != a[i]){
+            unionArr.push_back(a[i]);
+        }
+        i++;
+
+       }
+       else {
+        if(unionArr.size() == 0 || unionArr.back() != b[j]){
+            unionArr.push_back(b[j]);
+        }
+        j++;
+       }
+    }
+
+    while (i < n1){
+        if(unionArr.size() == 0 || unionArr.back() != a[i]){
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+
+    while (j < n2){
+        if(unionArr.size() == 0 || unionArr.back() != b[j]){
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+
+    return unionArr;
+
+}
+
+int main(){
+    vector<int> a = {1, 2, 3, 4, 5};
+    vector<int> b = {4, 5, 6, 7, 8};
+    
+    vector<int> result = unionOptimal(a, b);
+    
+    cout << "Union of a and b: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
