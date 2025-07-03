99b748e23f14796a54bf55a28a9be50bbaaf01fc
Manav Malhotra <roughsheet2002@gmail.com>
2025-07-03 10:24:56 +0530

    find k rotation in sorted array

---
+++ binary-search/number-of-times-sorted-arr-rotated.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findKRotation(vector<int> arr){
+    int n = arr.size();
+    int l = 0 , h = n-1 , s  = INT_MAX , ans = 0;
+    
+
+
+    while (l <= h)
+    {
+     int mid = (l+h)/2;
+     // right half sorted
+    if (arr[l] <= arr[h]) {
+            if (arr[low] < ans) {
+                ans = low;
+                s = arr[low];
+            }
+            break;
+        }
+
+     if(arr[mid] < arr[h]){
+        if(arr[mid] < s){
+           s = arr[mid];
+           ans = mid;
+        }
+        h = mid -1;
+     }
+     // left half sorted
+     else {
+       if(arr[l] < s){
+          s = arr[l];
+           ans = l;
+       }
+       l = mid +1 ;
+     }
+    }
+    
+
+
+
+    return ans;
+}
+
+int main() {
+
+
+    return 0;
+}
