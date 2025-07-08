7004153a5ee267480490551654435131bf2896cc
Manav Malhotra <roughsheet2002@gmail.com>
2025-07-08 09:10:28 +0530

    nth root of a number using bs

---
+++ binary-search/nth-root-of-a-number-using-bs.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+
+int findRoot(int n,int m) {
+   int l = 1 , h = n;
+
+   while(l <=h) {
+
+    int mid  = (l+h)/2;
+
+    if(pow(mid,n) == m){
+        return mid;
+    }
+    if(pow(mid,n) < m){
+      l = mid +1;
+    }
+    else {
+      h=mid-1;
+    }
+
+
+   }
+   return -1;
+
+
+}
+
+
+int main() {
+
+
+  
+    return 0;
+}
